1ade8a5f40a23806d71a5eb8b44d3e0168d747cd
Walkley He <walkleyhe@gmail.com>
2023-06-13 12:13:15 +0800

    Fix issue #1, stop kubelet.service and clean up existing images to avoid capability issue for pause image with kubelet.

--- snapshot.sh
+++ snapshot.sh
@@ -48,6 +48,7 @@ set -u
 AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-}
 AMI_ID=${AMI_ID:-/aws/service/bottlerocket/aws-k8s-1.24/x86_64/latest/image_id}
 INSTANCE_TYPE=${INSTANCE_TYPE:-t2.small}
+CTR_CMD="apiclient exec admin sheltie ctr -a /run/containerd/containerd.sock -n k8s.io"
 
 if [ -z "${AWS_DEFAULT_REGION}" ]; then
     echo "Please set AWS region"
@@ -66,14 +67,14 @@ export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
 export AWS_PAGER=""
 
 # launch EC2
-echo "[1/6] Deploying EC2 CFN stack ..."
+echo "[1/8] Deploying EC2 CFN stack ..."
 CFN_STACK_NAME="Bottlerocket-ebs-snapshot"
 aws cloudformation deploy --stack-name $CFN_STACK_NAME --template-file ebs-snapshot-instance.yaml --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides AmiID=$AMI_ID InstanceType=$INSTANCE_TYPE
 INSTANCE_ID=$(aws cloudformation describe-stacks --stack-name $CFN_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='InstanceId'].OutputValue" --output text)
 
 # wait for SSM ready
-echo -n "[2/6] Launching SSM ."
+echo -n "[2/8] Launching SSM ."
 while [[ $(aws ssm describe-instance-information --filters "Key=InstanceIds,Values=$INSTANCE_ID" --query "InstanceInformationList[0].PingStatus" --output text) != "Online" ]]
 do
    echo -n "."
@@ -81,8 +82,26 @@ do
 done
 echo " done!"
 
+# stop kubelet.service
+echo -n "[3/8] Stopping kubelet.service .."
+CMDID=$(aws ssm send-command --instance-ids $INSTANCE_ID \
+    --document-name "AWS-RunShellScript" --comment "Stop kubelet" \
+    --parameters commands="apiclient exec admin sheltie systemctl stop kubelet" \
+    --query "Command.CommandId" --output text)
+aws ssm wait command-executed --command-id "$CMDID" --instance-id $INSTANCE_ID > /dev/null
+echo " done!"
+
+# cleanup existing images
+echo -n "[4/8] Cleanup existing images .."
+CMDID=$(aws ssm send-command --instance-ids $INSTANCE_ID \
+    --document-name "AWS-RunShellScript" --comment "Cleanup existing images" \
+    --parameters commands="$CTR_CMD images rm \$($CTR_CMD images ls -q)" \
+    --query "Command.CommandId" --output text)
+aws ssm wait command-executed --command-id "$CMDID" --instance-id $INSTANCE_ID > /dev/null
+echo " done!"
+
 # pull images
-echo "[3/6] Pulling ECR images:"
+echo "[5/8] Pulling ECR images:"
 for IMG in "${IMAGES_LIST[@]}"
 do
     ECR_REGION=$(echo $IMG | sed -n "s/^[0-9]*\.dkr\.ecr\.\([a-z1-9-]*\)\.amazonaws\.com.*$/\1/p")
@@ -92,7 +111,7 @@ do
         echo -n "  $IMG - $PLATFORM ... "
         CMDID=$(aws ssm send-command --instance-ids $INSTANCE_ID \
             --document-name "AWS-RunShellScript" --comment "Pull Images" \
-            --parameters commands="apiclient exec admin sheltie ctr -a /run/dockershim.sock -n k8s.io images pull --platform $PLATFORM $IMG $ECRPWD" \
+            --parameters commands="$CTR_CMD images pull --platform $PLATFORM $IMG $ECRPWD" \
             --query "Command.CommandId" --output text)
         aws ssm wait command-executed --command-id "$CMDID" --instance-id $INSTANCE_ID > /dev/null && echo "done"
     done
@@ -100,18 +119,18 @@ done
 echo " done!"
 
 # stop EC2
-echo -n "[4/6] Stopping instance ... "
+echo -n "[6/8] Stopping instance ... "
 aws ec2 stop-instances --instance-ids $INSTANCE_ID --output text > /dev/null
 aws ec2 wait instance-stopped --instance-ids "$INSTANCE_ID" > /dev/null && echo "done!"
 
 # create EBS snapshot
-echo -n "[5/6] Creating snapshot ... "
+echo -n "[7/8] Creating snapshot ... "
 DATA_VOLUME_ID=$(aws ec2 describe-instances  --instance-id $INSTANCE_ID --query "Reservations[0].Instances[0].BlockDeviceMappings[?DeviceName=='/dev/xvdb'].Ebs.VolumeId" --output text)
 SNAPSHOT_ID=$(aws ec2 create-snapshot --volume-id $DATA_VOLUME_ID --description "Bottlerocket Data Volume snapshot" --query "SnapshotId" --output text)
 aws ec2 wait snapshot-completed --snapshot-ids "$SNAPSHOT_ID" > /dev/null && echo "done!"
 
 # destroy temporary instance
-echo "[6/6] Cleanup."
+echo "[8/8] Cleanup."
 aws cloudformation delete-stack --stack-name "Bottlerocket-ebs-snapshot"
 
 # done!
